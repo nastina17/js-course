1fa4aef19f68b65a39b1b575a5a829faa5bea3f8
Anastasiia Solodina <nastina17@gmail.com>
2026-08-23 16:29:37 +0300

    Unit-11 fixed2

--- tests/unit11.spec.ts
+++ tests/unit11.spec.ts
@@ -6,14 +6,15 @@ import { ProductPage } from 'pages/product.page';
 test('Verify user can view product details', async ({ page }) => {
     const homePage = new HomePage(page);
     const productPage = new ProductPage(page);
+    const productName = 'Combination Pliers';
 
     await page.goto('/');
 
-    await homePage.selectProduct('Combination Pliers');
+    await homePage.selectProduct(productName);
 
     await expect(page).toHaveURL(/product/);
 
-    await expect(productPage.productName).toHaveText('Combination Pliers');
+    await expect(productPage.productName).toHaveText(productName);
     await expect(productPage.productPrice).toHaveText('14.15');
 
     await expect(productPage.addToCartButton).toBeVisible();
